2e9ed91e5f72f4e4051533c8e58790d32a3d48b6
binisterlab <ilbuonbini@gmail.com>
2021-04-16 12:50:07 +0200

    Several fixes and LockedCamera Added
    -fixed small issues
    -added better camera system

---
+++ Assets/Scripts/LockedCameraFollow.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class LockedCameraFollow : MonoBehaviour
+{
+    Transform target;
+    [Range(1, 10)]
+    public float smoothFactor;
+    public Vector2 offset;
+
+    public bool ForwardOnly;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target != null && ForwardOnly)
+        {
+            Vector3 playerViewportPos = Camera.main.WorldToViewportPoint(target.transform.position + (Vector3)offset);
+            Debug.Log(playerViewportPos);
+            if (playerViewportPos.x > 0.5)
+            {
+                Follow();
+            }
+        }
+        else if(target != null && !ForwardOnly)
+        {
+            Follow();
+        }
+    }
+
+    void Follow()
+    {
+        Vector3 targetPos = target.position + (Vector3)offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position,targetPos,Time.deltaTime *smoothFactor);
+        smoothPosition.z = -10;
+        transform.position = smoothPosition;
+    }
+
+
+}
